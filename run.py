6d016ad4b6e2708c759fd401278ba6feae941166
M.Haris Noori <harisnoori20@gmail.com>
2023-06-25 01:22:50 +0100

    board number print logic created

--- run.py
+++ run.py
@@ -3,12 +3,22 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 def print_board(xState, zState):
+    zero = {'X' if xState[0] else ('O' if zState[0] else 0)}
+    one = {'X' if xState[1] else ('O' if zState[1] else 1)}
+    two = {'X' if xState[2] else ('O' if zState[2] else 2)}
+    three = {'X' if xState[3] else ('O' if zState[3] else 3)}
+    four = {'X' if xState[4] else ('O' if zState[4] else 4)}
+    five = {'X' if xState[5] else ('O' if zState[5] else 5)}
+    six = {'X' if xState[6] else ('O' if zState[6] else 6)}
+    seven = {'X' if xState[7] else ('O' if zState[7] else 7)}
+    eight = {'X' if xState[8] else ('O' if zState[8] else 8)}
+
     print(f"=============")
-    print(f"| 0 | 1 | 2 |")
+    print(f"| {zero} | {one} | {two} |")
     print(f"=============")
-    print(f"| 3 | 4 | 5 |")
+    print(f"| {three} | {four} | {five} |")
     print(f"=============")
-    print(f"| 6 | 7 | 8 |")
+    print(f"| {six} | {seven} | {eight} |")
     print(f"=============")
 
 
